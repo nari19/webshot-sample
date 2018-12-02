a7a555c0a25f8b19d1ef906a3716e61e5a6ddc8a
nari19 <kazuki7793996@gmail.com>
2018-12-03 08:43:48 +0900

    first commit

--- webshot.js
+++ webshot.js
@@ -1,4 +1,23 @@
 // webshot.js   https://www.actzero.jp/developer/report-20203.html      http://blog.honjala.net/entry/blog/2015/05/05/post-539/
+// http-server -o
+// node webshot.js
 const webshot = require('webshot');
-const link = 'https://af.nari19.me/border_stem/BorderStem.svg?a=HELLO%20WORLD&b=burdensome%20tasks..';
-webshot( link,'../activeframe2.png', function(err) {});
+
+var jikan= new Date();
+const year = jikan.getFullYear();
+const month = jikan.getMonth()+1;
+const week = jikan.getDay();
+const day = jikan.getDate();
+const hour = jikan.getHours();
+const minute = jikan.getMinutes();
+const second = jikan.getSeconds();
+
+const link = 'https://af.nari19.me/border_stem/BorderStem3.svg';
+
+const name1 = 'borders_stem3.png';
+const path1 = '../' + name1;
+webshot(link,path1, function(err) {});
+
+// const name2 = 'illust_suit.svg';
+// const path2 = '../' + name2;
+// webshot(link,path2, function(err) {});
